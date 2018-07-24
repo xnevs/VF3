f047871353946ba3582fb70061a8dfd4d765174c
Sven Cerk <sven.cerk@gmail.com>
2018-07-24 12:40:02 +0200

    add BinaryGraphLoader

--- include/argloader.hpp
+++ include/argloader.hpp
@@ -25,6 +25,8 @@
 #ifndef ARGLOADER_H
 #define ARGLOADER_H
 
+#include <cstddef>
+
 #include <iostream>
 #include <strstream>
 #include <map>
@@ -258,5 +260,38 @@ write(ostream &out, ARGLoader<Node,Edge> &g)
           }
       }
   }
+  
+
+
+class BinaryGraphLoader : public ARGEdit<int,Empty> {
+ public:
+  BinaryGraphLoader(istream & in) {
+    int cnt = read_word(in);
+    if(cnt <= 0) {
+      cnt = 0;
+      return;
+    }
+    int i;
+    for(i=0; i<cnt; ++i) {
+      int nattr = 0;
+      this->InsertNode(nattr);
+    }
+    for(i=0; i<cnt; ++i) {
+      int ecount, j;
+      ecount = read_word(in);
+      for(j=0; j<ecount; ++j) {
+        node_id v = read_word(in);
+        Empty eattr;
+        this->InsertEdge(i,v,eattr);
+      }
+    }
+  }
+ private:
+  uint16_t read_word(std::istream & in) {
+    uint16_t x = static_cast<unsigned char>(in.get());
+    x |= static_cast<uint16_t>(in.get()) << 8;
+    return x;
+  }
+};
 
 #endif
